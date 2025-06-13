36cd00f3408fe6bc45e4161c68b1cd1bd8944175
John-SSL <jsscadden@gmail.com>
2025-06-13 18:21:21 +0100

    Install whitenoise package

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@^96sz+k(r9&&cq#_+g8_m7ogjjzwxpoubpkz38)$lwk&)eo6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
